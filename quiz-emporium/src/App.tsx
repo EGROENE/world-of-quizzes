4305df6980563900fd20289f54aef1e93e779f4e
Ethan Groene <ethangroene@gmail.com>
2025-12-20 18:57:37 -0500

    Add Routes for existing pages

--- quiz-emporium/src/App.tsx
+++ quiz-emporium/src/App.tsx
@@ -1,13 +1,20 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import { Routes, Route } from "react-router";
+import "./App.css";
+import Homepage from "./Components/Homepage/Homepage";
+import LoginPage from "./Components/LoginPage/LoginPage";
+import SignupPage from "./Components/SignupPage/SignupPage";
 
 function App() {
-
   return (
     <>
-      
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignupPage />} />
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
