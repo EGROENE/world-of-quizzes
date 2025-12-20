0a493509c82174250c97d7c56109385d663884a9
Ethan Groene <ethangroene@gmail.com>
2025-12-20 18:58:29 -0500

    Add Links to Login,Signup pages

--- quiz-emporium/src/Components/Homepage/Homepage.tsx
+++ quiz-emporium/src/Components/Homepage/Homepage.tsx
@@ -1,8 +1,15 @@
+import { Link } from "react-router";
+
 const Homepage = () => {
   return (
     <>
       <h1>Welcome to the Quiz Emporium!</h1>
-      <button>Log In</button>
+      <Link to="/login">
+        <button>Log In</button>
+      </Link>
+      <Link to="/signup">
+        <button>Sign Up</button>
+      </Link>
       <button>Play as Guest</button>
     </>
   );
