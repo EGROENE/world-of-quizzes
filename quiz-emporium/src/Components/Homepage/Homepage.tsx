afd317201f4e82563ee14962ff2dbd852d00a9ba
Ethan Groene <ethangroene@gmail.com>
2025-12-20 18:11:53 -0500

    Init Homepage component

---
+++ quiz-emporium/src/Components/Homepage/Homepage.tsx
@@ -0,0 +1,10 @@
+const Homepage = () => {
+  return (
+    <>
+      <h1>Welcome to the Quiz Emporium!</h1>
+      <button>Log In</button>
+      <button>Play as Guest</button>
+    </>
+  );
+};
+export default Homepage;
